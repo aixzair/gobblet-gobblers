f26a4e48720c9bba2da59f7df9a9cb01037b054d
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-10-11 20:42:46 +0200

    feat : plateau js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 /* Constantes ----------------------------------------------------- */
 const PLATEAU = document.getElementById("plateau_id");
+const NB_COLONNE = 3;
+const NB_LIGNE = 3;
 
 /* Variables ------------------------------------------------------ */
 let plateau;
 
-
 /* Démarage ------------------------------------------------------- */
 creerPlateauHTML();
+initialiserPlateauJS();
 
 /* Evènements ----------------------------------------------------- */
 
@@ -17,11 +19,11 @@ function creerPlateauHTML() {
     let lig;
     let col;
 
-    for (lig = 0; lig < 3; lig++) {
+    for (lig = 0; lig < NB_LIGNE; lig++) {
         ligne = document.createElement("div");
         ligne.classList.add("ligne_c");
 
-        for (col = 0; col < 3; col++) {
+        for (col = 0; col < NB_COLONNE; col++) {
             cellule = document.createElement("div");
             cellule.classList.add("cellule_c");
             cellule.dataset.ligne = lig;
@@ -31,6 +33,15 @@ function creerPlateauHTML() {
         }
 
         PLATEAU.appendChild(ligne);
+    }
+}
+
+function initialiserPlateauJS() {
+    let col;
+    for (let lig = 0; lig < NB_LIGNE; lig++) {
+        for (col = 0; col < NB_COLONNE; col++) {
+            col[lig][col] = null;
+        }
     }
 }
 
