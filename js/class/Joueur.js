d1ca3f19923551d3882170b567a6effcb5421bd3
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-10-13 08:05:48 +0200

    feat : constructeur joueur

--- js/class/Joueur.js
+++ js/class/Joueur.js
@@ -1,3 +1,20 @@
+// require Enum.js
+
 class Joueur {
-    
+    #pions;
+
+    constructor(couleur) {
+        let i;
+        const couleurs_k = Object.keys(COULEURS);
+
+        if (!Object.values(COULEURS).includes(couleur)) {
+            throw new TypeError(`couleur invalide (construtor Joueur)`);
+        }
+
+        for (i = 0; i < couleurs_k.length; i++) {
+            this.#pions[couleurs_k[i]] = 2;
+        }
+
+        console.log(this.#pions);
+    }
 }
