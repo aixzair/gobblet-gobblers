7a3c7888e6820efe261d48128613c25ce4c31d8e
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-10-13 08:57:20 +0200

    perf: constructor Joueur

--- js/class/Joueur.js
+++ js/class/Joueur.js
@@ -4,18 +4,17 @@ class Joueur {
     #pions;
 
     constructor(couleur) {
-        let i;
-        const tailles_k = Object.keys(TAILLES);
-
         if (!Object.values(COULEURS).includes(couleur)) {
             throw new TypeError(`couleur invalide (construtor Joueur)`);
         }
 
         this.#pions = {};
-        for (i = 0; i < tailles_k.length; i++) {
-            this.#pions[tailles_k[i]] = 2;
+        for (const key in TAILLES) {
+            this.#pions[key] = 2;
         }
+    }
 
-        console.log(this.#pions);
+    getPions() {
+        return this.#pions;
     }
 }
